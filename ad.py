3885c53865f93ac19c63d0952135b444f4f32784
Jane <42863214+qwertyuiop1357@users.noreply.github.com>
2019-03-04 12:15:00 -0500

    Update ad.py

--- ad.py
+++ ad.py
@@ -22,39 +22,3 @@ CONTINUOUS_FEATURES = ['item_price_level', 'item_sales_level', 'item_collected_l
 
 VECTOR_FEATURES = ['item_category_list']
 
-def _pop_features(df):
-    for feature in POP_FEATURES:
-        df.pop(feature)
-
-def _generate_categorical_features(df, CATEGORICAL_FEATURES):
-    """
-    categorical features
-    """
-    oh_encoder = OneHotEncoder(sparse=True, categories='auto')
-    space = oh_encoder.fit_transform(df[CATEGORICAL_FEATURES[0]].values.reshape((-1, 1))).toarray()
-    for feature in CATEGORICAL_FEATURES[1:]:
-        val = oh_encoder.fit_transform(df[feature].values.reshape((-1, 1))).toarray()
-        space = np.hstack((space, val))
-    return space
-
-
-def _generate_vector_features(df, VECTOR_FEATURES):
-    """
-    vector features
-    """
-    cv = CountVectorizer()
-    for feature in VECTOR_FEATURES:
-        val = cv.fit_transform(df[feature]).toarray()
-    return val
-
-
-if __name__ == '__main__':
-    df = pd.read_csv(DATA_PATH, sep=' ', nrows=100000)
-    df['date'] = df['context_timestamp'].apply(lambda x: time.strftime('%Y-%m-%d', time.localtime(x)))
-    df = _generate_historical_convrate(df)
-    df = _generate_instant_feature(df)
-    y = df.pop('is_trade')
-    _pop_features(df)
-    df = _generate_categorical_features(df, CATEGORICAL_FEATURES)
-    X = _generate_vector_features(df, VECTOR_FEATURES)
-   
